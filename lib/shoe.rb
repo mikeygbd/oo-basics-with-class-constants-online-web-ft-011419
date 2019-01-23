a98f10009d87c32d40b3d00abdaa1530e2a1d71c
Michael Soares <mikey.soares@gmail.com>
2019-01-23 01:44:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,13 +5,7 @@ class Shoe
 
   BRANDS = []
 
-  # def initialize(brand)
-  #   @brand = brand
-  #   BRANDS << brand
-  #   if BRANDS.length > 3
-  #     BRANDS.pop
-  #   end
-  # end
+
   def initialize(brand)
       @brand = brand
       if !BRANDS.include?(brand)
